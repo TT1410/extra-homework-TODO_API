476aec6118e1a008fdd937f0d0b1dec77c047841
TT1410 <112493080+TT1410@users.noreply.github.com>
2022-10-05 18:48:04 +0300

    Update admin.py

--- TODO_API/apps/accounts/admin.py
+++ TODO_API/apps/accounts/admin.py
@@ -1,5 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-from .models import User
+User = get_user_model()
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('id', 'email', 'is_superuser', 'is_active')
+    search_fields = ('id', 'email', 'first_name', 'last_name')
+    list_filter = ('is_superuser', 'is_active')
+
+    add_fieldsets = (
+        (
+            None, {
+                'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2'),
+            },
+        ),
+    )
+
+    fieldsets = (
+        (
+            _('Login data'), {
+                'fields': ('email', 'password'),
+            },
+        ),
+        (
+            _('Personal info'), {
+                'fields': ('first_name', 'last_name'),
+            },
+        ),
+        (
+            _('Permissions'), {
+                'fields': ('is_active', 'is_superuser'),
+            },
+        ),
+    )
+
+    ordering = ('email',)
