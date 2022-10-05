9f538011c6e6e839dea1403b93b3370b5f50fa9f
TT1410 <112493080+TT1410@users.noreply.github.com>
2022-10-05 18:26:10 +0300

    Task 2.2
    1. Заміна HttpResponse на JsonResponse.
    2. Додано адмінку до юзера

--- TODO_API/apps/accounts/views.py
+++ TODO_API/apps/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 def index(request):
-    return HttpResponse("<b>Hello, world. You're at the polls index.</b>")
+    return JsonResponse({"message": "Hello, world. You're at the polls index."})
